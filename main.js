019f0d3cab4b479c5303e4c3edd69daecdaa5e9e
Saket578 <131536130+Saket578@users.noreply.github.com>
2023-11-13 20:35:18 +0530

    Update main.js
    Changes made on the basis of part II of the project...

--- main.js
+++ main.js
@@ -8,6 +8,23 @@ function setup(){
     video = createCapture(VIDEO);
     video.size(300, 300);
     video.hide();
+
+    poseNet = ml5.poseNet(video, function(){
+        // model loaded
+        console.log("Model has been loaded!"+ " ml5 version: "+ ml5.version);
+        poseNet.on("pose", function(results){
+            // got poses
+            if(!results.length > 0){
+                console.log("No face detection");
+            }else{
+                console.log(results);
+
+                x = results[0].pose.nose.x;
+                y = results[0].pose.nose.y;
+                console.log("X: "+ x+ "Y : "+ y);
+            }
+        });
+    });
 }
 
 function draw(){
