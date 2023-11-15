714c0b8d8e030ca5ed847898afdfc82c491ea18c
Saket578 <131536130+Saket578@users.noreply.github.com>
2023-11-15 13:31:09 +0530

    Update main.js
    wsff

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+var x = 0;
+var y = 0;
+
 function preload(){
-    
+    moustache = loadImage("mousetace.png");
 }
 
 function setup(){
@@ -14,21 +17,22 @@ function setup(){
         console.log("Model has been loaded!"+ " ml5 version: "+ ml5.version);
         poseNet.on("pose", function(results){
             // got poses
-            if(!results.length > 0){
-                console.log("No face detection");
-            }else{
+            if(results.length > 0){
                 console.log(results);
 
                 x = results[0].pose.nose.x;
                 y = results[0].pose.nose.y;
                 console.log("X: "+ x+ "Y : "+ y);
+            }else{
+                console.log("No face detection");
             }
         });
     });
 }
 
 function draw(){
-    image(video, 0, 0, canvas.width, canvas.height);
+        image(video, 0, 0, canvas.width, canvas.height);
+        image(moustache, x+60, y+100, 100, 50);     
 }
 
 function svFlt(){
